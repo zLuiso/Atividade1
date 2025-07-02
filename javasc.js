963f49979e51ab1729b88cfb4b6ac3ba86a9104a
zLuiso <luiis.gustaavo@gmail.com>
2025-07-01 22:28:49 -0300

    Add files via upload

---
+++ javasc.js
@@ -0,0 +1,34 @@
+
+var counter = 0
+
+function countingClicks(){
+	document.getElementById("botaoz").innerHTML= ++counter
+	document.getElementById("botaoz").disabled =true
+
+botao.onclick = function() {
+      alert("Bem vindoooo");
+
+}}
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+   
+   
+      
+   
+     
